888db0f7f18214399ee3960200ed361fcccf80ae
Anthony Wong <wongelz@gmail.com>
2019-10-29 23:03:56 +1100

    Update zio to 1.0.0-RC15

--- build.sbt
+++ build.sbt
@@ -4,13 +4,13 @@ name := "zio-scalajs-solarsystem"
 
 version := "0.1"
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.13.0"
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
 
 libraryDependencies ++= Seq(
-  "org.scala-js" %%% "scalajs-dom" % "0.9.6",
-  "org.scalaz" %%% "scalaz-zio" % "1.0-RC4",
+  "org.scala-js" %%% "scalajs-dom" % "0.9.7",
+  "dev.zio" %%% "zio" % "1.0.0-RC15",
   "org.scala-js" %%% "scalajs-java-time" % "0.2.5"
 )
--- src/main/scala/SolarSystemExample.scala
+++ src/main/scala/SolarSystemExample.scala
@@ -1,7 +1,7 @@
 import org.scalajs.dom.document
 import org.scalajs.dom.html.{Canvas, Image}
-import scalaz.zio.duration._
-import scalaz.zio.{App, IO, UIO, ZSchedule}
+import zio.duration._
+import zio.{App, UIO, ZSchedule}
 
 import scala.scalajs.js.Date
 
@@ -17,7 +17,7 @@ object SolarSystemExample extends App {
     for {
       s <- init
       _ <- draw(s).repeat(ZSchedule.fixed(10.millisecond))
-    } yield ()
+    } yield 0
 
   def init: UIO[SolarSystem] =
     for {
@@ -28,43 +28,43 @@ object SolarSystemExample extends App {
 
   def draw(s: SolarSystem): UIO[Unit] = {
     for {
-      c <- IO.effectTotal { document.getElementById("canvas").asInstanceOf[Canvas].getContext("2d") }
+      c <- UIO.effectTotal { document.getElementById("canvas").asInstanceOf[Canvas].getContext("2d") }
 
-      _ <- IO.effectTotal { c.globalCompositeOperation = "destination-over" }
-      _ <- IO.effectTotal { c.clearRect(0, 0, 300, 300) }
+      _ <- UIO.effectTotal { c.globalCompositeOperation = "destination-over" }
+      _ <- UIO.effectTotal { c.clearRect(0, 0, 300, 300) }
 
-      _ <- IO.effectTotal { c.fillStyle = "rgba(0, 0, 0, 0.4)"}
-      _ <- IO.effectTotal { c.strokeStyle = "rgba(0, 153, 255, 0.4)"}
-      _ <- IO.effectTotal { c.save() }
-      _ <- IO.effectTotal { c.translate(150, 150) }
+      _ <- UIO.effectTotal { c.fillStyle = "rgba(0, 0, 0, 0.4)"}
+      _ <- UIO.effectTotal { c.strokeStyle = "rgba(0, 153, 255, 0.4)"}
+      _ <- UIO.effectTotal { c.save() }
+      _ <- UIO.effectTotal { c.translate(150, 150) }
 
       // earth
-      t <- IO.effectTotal { new Date() }
-      _ <- IO.effectTotal { c.rotate(((2 * Math.PI) / 60) * t.getSeconds() + ((2 * Math.PI) / 60000) * t.getMilliseconds()) }
-      _ <- IO.effectTotal { c.translate(105, 0) }
-      _ <- IO.effectTotal { c.fillRect(0, -12, 40, 24) } // shadow
-      _ <- IO.effectTotal { c.drawImage(s.earth, -12, -12) }
+      t <- UIO.effectTotal { new Date() }
+      _ <- UIO.effectTotal { c.rotate(((2 * Math.PI) / 60) * t.getSeconds() + ((2 * Math.PI) / 60000) * t.getMilliseconds()) }
+      _ <- UIO.effectTotal { c.translate(105, 0) }
+      _ <- UIO.effectTotal { c.fillRect(0, -12, 40, 24) } // shadow
+      _ <- UIO.effectTotal { c.drawImage(s.earth, -12, -12) }
 
       // moon
-      _ <- IO.effectTotal { c.save() }
-      _ <- IO.effectTotal { c.rotate(((2 * Math.PI) / 6) * t.getSeconds() + ((2 * Math.PI) / 6000) * t.getMilliseconds())}
-      _ <- IO.effectTotal { c.translate(0, 28.5) }
-      _ <- IO.effectTotal { c.drawImage(s.moon, -3.5, -3.5) }
-      _ <- IO.effectTotal { c.restore() }
+      _ <- UIO.effectTotal { c.save() }
+      _ <- UIO.effectTotal { c.rotate(((2 * Math.PI) / 6) * t.getSeconds() + ((2 * Math.PI) / 6000) * t.getMilliseconds())}
+      _ <- UIO.effectTotal { c.translate(0, 28.5) }
+      _ <- UIO.effectTotal { c.drawImage(s.moon, -3.5, -3.5) }
+      _ <- UIO.effectTotal { c.restore() }
 
-      _ <- IO.effectTotal { c.restore() }
+      _ <- UIO.effectTotal { c.restore() }
 
-      _ <- IO.effectTotal { c.beginPath() }
-      _ <- IO.effectTotal { c.arc(150, 150, 105, 0, Math.PI * 2, false) } // earth orbit
-      _ <- IO.effectTotal { c.stroke() }
+      _ <- UIO.effectTotal { c.beginPath() }
+      _ <- UIO.effectTotal { c.arc(150, 150, 105, 0, Math.PI * 2, false) } // earth orbit
+      _ <- UIO.effectTotal { c.stroke() }
 
-      _ <- IO.effectTotal { c.drawImage(s.sun, 0, 0, 300, 300) }
+      _ <- UIO.effectTotal { c.drawImage(s.sun, 0, 0, 300, 300) }
     } yield ()
   }
 
 
   private def loadImage(src: String) =
-    IO.effectTotal {
+    UIO.effectTotal {
       val image = document.createElement("img").asInstanceOf[Image]
       image.src = src
       image
